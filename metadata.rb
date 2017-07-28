2d75b7b1612c54cd449ee97c45201ed39d4af7f3
Alex Vernacchia <avernacchia@salesforce.com>
2017-07-28 09:45:56 +0100

    Release 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'alvernacchia@gmail.com'
 license          'All rights reserved'
 description      'handle tentacle registration for linux ssh servers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.1.0'
 chef_version     '~> 12'
 issues_url       'https://github.com/vernak2539/octopus-tentacle-registration/issues'
 source_url       'https://github.com/vernak2539/octopus-tentacle-registration'
